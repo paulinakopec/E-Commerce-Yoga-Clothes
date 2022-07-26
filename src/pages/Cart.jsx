bfbe05b5b94cd55215541401dbe832971130619d
Paulina Kopec <100046393+paulinakopec@users.noreply.github.com>
2022-07-26 14:25:54 +0200

    Cart file update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import BaliShorts from '../img/Bali_shorts.jpeg'
 
 const Container = styled.div``
 
@@ -41,7 +42,39 @@ const TopText = styled.span`
 `
 
 const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const Info = styled.div`
+    flex: 3;
+`
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`
+const Image = styled.img`
+    width: 200px;
+`
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
+const ProductName = styled.span``
+const ProductId = styled.span``
+const ProductColor = styled.div``
+const ProductSize = styled.span``
+const PriceDetail = styled.span`
+    flex: 1;
+`
 
+const Summary = styled.div`
+    flex: 1;
 `
 
 const Cart = () => {
@@ -61,7 +94,23 @@ const Cart = () => {
                 </TopTexts>
                 <TopButton type="filled">CHECKOUT NOW</TopButton>
             </Top>
-            <Bottom></Bottom>
+            <Bottom>
+                <Info>
+                    <Product>
+                        <ProductDetail>
+                            <Image src={BaliShorts}></Image>
+                            <Details>
+                                <ProductName><b>Product:</b> BALI TOP</ProductName>
+                                <ProductId><b>ID:</b> 85897389492</ProductId>
+                                <ProductColor></ProductColor>
+                                <ProductSize><b> Size:</b> M</ProductSize>
+                            </Details>
+                        </ProductDetail>
+                        <PriceDetail>189 PLN</PriceDetail>
+                    </Product>
+                </Info>
+                <Summary>summary</Summary>
+            </Bottom>
         </Wrapper>
         <Footer></Footer>
     </Container>
