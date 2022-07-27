5795e89c33098f6ee7304d2ef615c592d122cff4
Paulina Kopec <100046393+paulinakopec@users.noreply.github.com>
2022-07-27 21:22:35 +0200

    Adding title and styling

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -6,6 +6,16 @@ import Product from './Product';
 const Container = styled.div`
     padding: 20px;
     display: flex;
+    flex-direction: column;
+`
+const Title = styled.h1`
+    margin-bottom: 10px;
+    text-transform: uppercase;
+    text-align: center;
+`
+
+const ProductItems = styled.div`
+    display: flex;
     flex-wrap: wrap;
     justify-content: space-between;
 `
@@ -13,9 +23,12 @@ const Container = styled.div`
 const Products = () => {
   return (
     <Container>
+        <Title>Featured Products</Title>
+        <ProductItems>
         {popularProducts.map((item) => (
             <Product item={item} key={item.id}/>
         ))}
+        </ProductItems>
     </Container>
   );
 };
