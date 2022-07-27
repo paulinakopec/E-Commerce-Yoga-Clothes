81abb9242f28b30a2ae6820d33e3c7d982a37828
Paulina Kopec <100046393+paulinakopec@users.noreply.github.com>
2022-07-27 10:49:19 +0200

    Mobile responsiveness update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import { Facebook, Instagram, Twitter, Pinterest, Room, Phone, MailOutline } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
+import { mobile } from '../responsive'; 
 
 const Container = styled.div`
     display: flex;
-
+    ${mobile({flexDirection: "column"})};
 `
 const Left = styled.div`
     flex: 1;
@@ -15,6 +16,7 @@ const Left = styled.div`
 const Center = styled.div`
     flex: 1;
     padding: 20px;
+    ${mobile({ display: "none"})};
 `
 
 const Title = styled.h3`
@@ -37,6 +39,7 @@ const ListItem = styled.li`
 const Right = styled.div`
     flex: 1;
     padding: 20px;
+    ${mobile ({backgroundColor: "ghostwhite"})};
 `
 const Logo = styled.h1``
 
