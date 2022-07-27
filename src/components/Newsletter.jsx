c4f09b93d90bcf67f1919a05529834c7999c4bf2
Paulina Kopec <100046393+paulinakopec@users.noreply.github.com>
2022-07-27 10:27:25 +0200

    Mobile responsiveness update

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,6 +1,7 @@
 import { Send } from '@material-ui/icons';
 import React from 'react';
 import styled from 'styled-components';
+import { mobile } from '../responsive';
 
 const Container = styled.div`
     height: 60vh;
@@ -18,7 +19,8 @@ const Title = styled.h1`
 const Description = styled.div`
     font-size: 24px;
     font-weight: 300;
-    margin-bottom: 20px;    
+    margin-bottom: 20px;
+    ${mobile({textAlign: "center"})};    
 `
 const InputContainer = styled.div`
     width: 50%;
@@ -27,6 +29,7 @@ const InputContainer = styled.div`
     display: flex;
     justify-content: space-between;
     border: 1px solid lightgray;
+    ${mobile({width: "80%"})}; 
 `
 const Input = styled.input`
     border: none;
